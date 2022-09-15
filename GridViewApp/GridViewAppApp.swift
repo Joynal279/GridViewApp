adf079446a782d2b2f24bb952a6e4012192ef21e
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-15 16:25:00 +0600

    Initial Commit

---
+++ GridViewApp/GridViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewAppApp.swift
+//  GridViewApp
+//
+//  Created by JOYNAL ABEDIN on 15/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GridViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
